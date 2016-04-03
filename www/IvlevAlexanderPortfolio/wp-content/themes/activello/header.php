524dba52cf803ffe62cf7454b459625015a83fd8
Ivlev Alexander <ivlev.stef@gmail.com>
2016-04-03 23:04:41 +0600

    Added About page. Removed graceless site themes.

--- www/IvlevAlexanderPortfolio/wp-content/themes/activello/header.php
+++ www/IvlevAlexanderPortfolio/wp-content/themes/activello/header.php
@@ -45,11 +45,6 @@
 						</div>
 						<?php activello_header_menu(); // main navigation ?>
 
-						<div class="nav-search"><?php 
-                            add_filter('get_search_form', 'activello_header_search_filter',10,3);
-                            echo get_search_form();
-                            remove_filter('get_search_form', 'activello_header_search_filter');?>							
-						</div>
 					</div>
 				</div>
 			</div>
